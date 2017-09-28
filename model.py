951f13b2e32a28b6b84d7ffb75a701d454fee706
Menno <menno.taanman@gmail.com>
2017-09-28 11:40:49 +0200

    BQ_import

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
-print('Hello')
+import uuid
+
+from google.cloud import bigquery 
+
+query = """SELECT * FROM Menno.pr_train LIMIT 10"""
+client = bigquery.Client()
+
+query_job = client.run_async_query(str(uuid,uuid4()), query)
+query_job.begin()
+data = query_job.result()
+
+for row in data:
+	print(row)

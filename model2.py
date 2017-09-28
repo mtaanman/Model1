5a83121a9dd8403aee9e8ac7ca9eded389cc4c39
Menno <menno.taanman@gmail.com>
2017-09-28 13:11:31 +0200

    BQ_import5

--- model2.py
+++ model2.py
@@ -2,11 +2,8 @@ import pandas as pd
 from pandas.io import gbq
 
 query = """SELECT * FROM [ziggo-big-query:Menno.pr_train] LIMIT 10"""
+pid = 'ziggo-big-query'
 
-try:
-	data = gbq.read_gbq(query)
-except:
-	print 'Query error'
+data = gbq.read_gbq(query,project_id=pid)
 
-for row in data:
-	print(row)
+print(data)
